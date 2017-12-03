b19966c28d01bbd6db298621ea76d6b96faac30e
Softiet <33199800+Softiet@users.noreply.github.com>
2017-12-04 00:04:26 +0800

    copy

---
+++ test.js
@@ -0,0 +1,6 @@
+console.log('5487');
+//window.alert('共產');
+//document.getElementById('choice').textContent= new Date();
+console.log(document.getElementById('heck'));
+
+  document.getElementById('heck').textContent= new Date();
